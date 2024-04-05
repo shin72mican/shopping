0240e34b88318fc7b88339168d550803b96f6a71
shin72mican <shin72mican@gmail.com>
2024-04-05 17:09:22 +0900

    管理者 - 商品管理 - 新規登録画像保存実装

--- shopping-app/src/main/java/jp/co/illmatics/apps/shopping/controller/AdminProductController.java
+++ shopping-app/src/main/java/jp/co/illmatics/apps/shopping/controller/AdminProductController.java
@@ -1,13 +1,22 @@
 package jp.co.illmatics.apps.shopping.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jp.co.illmatics.apps.shopping.entity.Categories;
@@ -78,6 +87,57 @@ public class AdminProductController {
 		model.addAttribute("product", products.get(0));
 		
 		return "admin/products/show";
+	}
+	
+	@GetMapping("/admin/products/create")
+	public String create(Model model) {
+		List<Categories> categories = categoriesMapper.findAll();
+		model.addAttribute("categories", categories);
+		return "admin/products/create";
+	}
+	
+	@PostMapping("/admin/products")
+	public String store(
+			@RequestParam(name = "category_id", defaultValue = "0") Long categoryId,
+			@RequestParam(name = "name", defaultValue = "") String name,
+			@RequestParam(name = "price", defaultValue = "0") Long price,
+			@RequestParam(name = "product_image", defaultValue="") MultipartFile productImage,
+			Model model) throws IOException {
+		
+		// エラーチェック
+		List<String> errors = new ArrayList<String>();
+		
+		if (name.equals("")) {
+			errors.add("名前を入力してください");
+		}
+		
+		if (price == null) {
+			errors.add("価格を入力してください");
+		}
+		
+		if (price < 0) {
+			errors.add("0以上で価格を入力して下さい");
+		}
+		
+		model.addAttribute("errors", errors);
+		
+		if(!productImage.isEmpty()) {
+			// 一意な画像ファイル名の作成
+			// ファイル名取得
+			String originalFileName = productImage.getOriginalFilename();
+			// ファイル拡張子取得
+			String extension = originalFileName.substring(originalFileName.lastIndexOf("."));
+			// 一意な文字列取得
+			UUID uuid = UUID.randomUUID();
+			// 新しいファイル名
+			String fileName = uuid.toString() + extension;
+			// 保存先
+			Path filePath=Paths.get("static/products/" + fileName);
+			// 保存
+			Files.copy(productImage.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+		}
+		
+		return "redirect:/admin/products";
 	}
 }
 
