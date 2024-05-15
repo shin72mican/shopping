87f061688a5e05b80d41cd1b399227fdd8d10300
shin72mican <shin72mican@gmail.com>
2024-05-15 11:38:30 +0900

    カテゴリ管理 - 修正

--- shopping-app/src/main/java/jp/co/illmatics/apps/shopping/service/admin/error/CategoryErrorCheckService.java
+++ shopping-app/src/main/java/jp/co/illmatics/apps/shopping/service/admin/error/CategoryErrorCheckService.java
@@ -3,23 +3,36 @@ package jp.co.illmatics.apps.shopping.service.admin.error;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+
+import jp.co.illmatics.apps.shopping.entity.Categories;
+import jp.co.illmatics.apps.shopping.mapper.CategoriesMapper;
 
 @Service
 public class CategoryErrorCheckService {
 	
-	public List<String> errorCheck(String name, Long orderNo) {
+	@Autowired
+	CategoriesMapper categoriesMapper;
+	
+	public List<String> errorCheck(Categories category) {
+		
 		List<String> errors = new ArrayList<String>();
 		
-		if (!StringUtils.hasLength(name)){
-			errors.add("名前を入力してください");
-		}
-			
-		if (orderNo == null) {
-			errors.add("並び順番号を入力してください");
-		} else if(orderNo <= 0) {
-			errors.add("1以上の並び順番号を入力してください");
+		List<Categories> checkCategories = categoriesMapper.find(category);
+		if (checkCategories.size() > 0 && category.getOrderNo() != null && category.getOrderNo() >= 0) {
+			errors.add("指定された並び順番号は既に存在します");
+		} else {
+			if (!StringUtils.hasLength(category.getName())){
+				errors.add("名前を入力してください");
+			}
+				
+			if (category.getOrderNo() == null) {
+				errors.add("並び順番号を入力してください");
+			} else if(category.getOrderNo() <= 0) {
+				errors.add("1以上の並び順番号を入力してください");
+			}
 		}
 		
 		return errors;
