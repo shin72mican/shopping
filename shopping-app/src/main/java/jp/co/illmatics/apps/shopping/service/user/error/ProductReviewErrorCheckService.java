300027dd56301525409bc499087134d0cfb75922
shin72mican <shin72mican@gmail.com>
2024-05-31 10:11:54 +0900

    商品レビュー - validate byte数255以下であるかのチェック

--- shopping-app/src/main/java/jp/co/illmatics/apps/shopping/service/user/error/ProductReviewErrorCheckService.java
+++ shopping-app/src/main/java/jp/co/illmatics/apps/shopping/service/user/error/ProductReviewErrorCheckService.java
@@ -1,5 +1,6 @@
 package jp.co.illmatics.apps.shopping.service.user.error;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,16 +19,20 @@ public class ProductReviewErrorCheckService {
 			errors.add("タイトルを入力して下さい");
 		}
 		
-		if(productReview.getTitle().length() > 255) {
-			errors.add("255文字を超えるタイトルを登録することはできません");
+		if(productReview.getTitle().getBytes(StandardCharsets.UTF_8).length > 255) {
+        	errors.add("半角の文字列であれば255文字、全角の文字列ならば127文字を超えるタイトルを登録することができません");
+        } else if(productReview.getTitle().length() > 255) {
+			errors.add("255文字を超えるタイトルを登録することができません");
 		}
 		
 		if(!StringUtils.hasLength(productReview.getBody())) {
 			errors.add("本文を入力してください");
 		}
 		
-		if(productReview.getBody().length() > 255) {
-			errors.add("255文字を超える本文を登録することはできません");
+		if(productReview.getBody().getBytes(StandardCharsets.UTF_8).length > 255) {
+        	errors.add("半角の文字列であれば255文字、全角の文字列ならば127文字を超える本文を登録することができません");
+        } else if(productReview.getBody().length() > 255) {
+			errors.add("255文字を超える本文を登録することができません");
 		}
 		
 		return errors;
